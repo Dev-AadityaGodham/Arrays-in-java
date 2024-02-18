66c099931e17d16c5553711ad4ed52c1faf8635a
Dev-AadityaGodham <159020029+Dev-AadityaGodham@users.noreply.github.com>
2024-02-18 22:39:55 +0530

    Create 2D_Array.java

---
+++ 2D_Array.java
@@ -0,0 +1,45 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc= new Scanner(System.in);
+	    System.out.println("Enter the size of row :");
+		int row =sc.nextInt();
+		System.out.println("Enter the size of Col :");
+		int col =sc.nextInt();
+		int[][]numbers = new int[row][col];
+		System.out.println("Which number you want to find :");
+		int x = sc.nextInt();
+		
+		
+		for(int i=0;i<row;i++){
+		    for(int j=0;j<col;j++){
+		        System.out.println("enter the "+i+"th and "+j+"th element");
+		        numbers[i][j]=sc.nextInt();
+		    }
+		}
+		System.out.println("The Array is:");
+		for(int i=0;i<row;i++){
+		    for(int j=0;j<col;j++){
+		       System.out.print(numbers[i][j]);
+		    }
+		    System.out.println();
+		}
+		for(int i=0;i<row;i++){
+		    for(int j=0;j<col;j++){
+		       if(numbers[i][j]==x){
+		           System.out.println("the "+x+" found at ( "+i+" , "+j+" ) Position");
+		       }
+		    }
+		    System.out.println();
+		}
+		
+	}
+}
