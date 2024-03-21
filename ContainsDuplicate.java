d889355e1a0142521ee0ae78695a4c2151c47a37
Dev-AadityaGodham <159020029+Dev-AadityaGodham@users.noreply.github.com>
2024-03-21 16:57:33 +0530

    Create ContainsDuplicate.java

---
+++ ContainsDuplicate.java
@@ -0,0 +1,24 @@
+/******************************************************************************
+Leetcode
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		int nums[]={1,2,3,1};
+		int counter =0;
+		for(int i = 0 ;i<nums.length-1;i++){
+            for(int j = i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    counter++;
+                }
+            }
+            
+        }
+        if(counter>=1){
+            System.out.println("True");
+        }else{
+            System.out.println("False");
+        }
+	}
+}
